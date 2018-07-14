d6ab6cb8b32002315d1d1177a8d2f434c507eb64
Richard Neese <r.neese@gmail.com>
2018-07-14 15:37:24 -0400

    new version

--- ubitx_20/ubitx.h
+++ ubitx_20/ubitx.h
@@ -24,12 +24,12 @@
 //==============================================================================
 //Depending on the type of LCD mounted on the uBITX, uncomment one of the options below.
 //You must select only one.
-#define UBITX_DISPLAY_LCD1602P        //LCD mounted on unmodified uBITX (Parallel)
+//#define UBITX_DISPLAY_LCD1602P        //LCD mounted on unmodified uBITX (Parallel)
 //#define UBITX_DISPLAY_LCD1602I        //I2C type 16 x 02 LCD
 //#define UBITX_DISPLAY_LCD1602I_DUAL   //I2C type 16 x02 LCD Dual
 //#define UBITX_DISPLAY_LCD2004P        //24 x 04 LCD (Parallel)
 //#define UBITX_DISPLAY_LCD2004I        //I2C type 24 x 04 LCD
-//#define UBITX_DISPLAY_NEXTION         //NEXTION LCD 
+#define UBITX_DISPLAY_NEXTION         //NEXTION LCD 
 //#define UBITX_CONTROL_MCU             //CONTROL MCU
 
 //#define UBITX_DISPLAY_NEXTION_SAFE      //Only EEProm Write 770~775
